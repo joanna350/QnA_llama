23cbae4ac7a8b259af25896432373a6d9f00ceaf
Joanna <joanna@kaist.ac.kr>
2024-09-01 20:56:02 +0300

    [core] app: qna with llm

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import chainlit as cl
+from langchain_community.llms import CTransformers
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+llm = "TheBloke/zephyr-7B-beta-GGUF"  # model to download from
+
+config = {
+    "max_new_tokens": 1024, # curtails the output length
+    "repetition_penalty": 1.1,
+    "temperature": 0.5, # less determinstic with higher val
+    "top_k": 30, 
+    "top_p": 0.9,
+    "stream": True,
+    "threads": int(os.cpu_count() / 2)
+}
+
+llm_init = CTransformers(
+    model=llm,
+    model_type="zephyr",
+    lib="avx2",
+    **config
+)
+
+template = """Question: {question}
+
+Response: Please stay factual and omit fiction.
+"""
+
+@cl.on_chat_start
+def main():
+    prompt = PromptTemplate(template=template, input_variables=['question'])
+    llm_chain = prompt | llm_init
+    cl.user_session.set("llm_chain", llm_chain)
+
+@cl.on_message
+async def main(message: str):
+    llm_chain = cl.user_session.get("llm_chain")
+    res = await llm_chain.ainvoke(message.content)
+    await cl.Message(content=res).send()
---
+++ readme.md
@@ -0,0 +1,36 @@
+**Q&A with LLM**
+
+**Summary**
+
+The short script generates question-answering interface that leverages on recent LLM model. Libraries used are `chainlit`, `langchain`, and `CTransformers`.
+
+
+**How To**
+
+1. Install required modules
+   ```
+   python -m venv app-venv
+   python -m pip install -r requirements.txt
+   ```
+
+2. Run
+   ```
+   chainlit run app.py
+   ```
+
+**Notes**
+
+1. **Tuning**
+   - Parameters in the `config` dictionary are recommended to be adjusted for the desired behavior of the Q&A system. 
+
+2. **Format**
+   - By modifying the `template` variable, the format of the responses from the system will change.
+
+3. By updating the models, changing the configurations and template in accordance will keep your system generating responses that are relevant and appropriate
+
+4. By updating the Python version and the code syntax as modules are updated, it will produce more timely responses
+
+**Demo**
+![run.png](img/run2.png)
+
+- time taken: 1 minute +/- 30 seconds depending on the prompt
